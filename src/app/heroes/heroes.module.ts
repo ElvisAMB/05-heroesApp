c67940218fcfd0ca39a1189ccebf9a6e0aca67d0
Elvis Mora <pegazuz@live.com>
2021-03-29 22:48:56 -0500

    Creación de rutas hijas

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -3,20 +3,18 @@ import { CommonModule } from '@angular/common';
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
-import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
-
-
+import { HeroesRoutingModule } from './heroes-routing.module';
 
 @NgModule({
   declarations: [
     AgregarComponent, 
     BuscarComponent, 
     HeroeComponent, 
-    HomeComponent, 
     ListadoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HeroesRoutingModule
   ]
 })
 export class HeroesModule { }

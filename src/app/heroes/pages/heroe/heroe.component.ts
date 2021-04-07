f615fbfde9edd275c46fa177f2f539a7c35c9e2c
Elvis Mora <pegazuz@live.com>
2021-04-06 20:23:40 -0500

    Diseño de la pantalla de heroe

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { HeroesService } from '../../services/heroes.service';
 import { Heroe } from '../../interfaces/heroes.interface';
 import { switchMap, tap } from 'rxjs/operators';
@@ -7,18 +7,27 @@ import { switchMap, tap } from 'rxjs/operators';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [
+  styles: [`
+    img {
+      width: 100%;
+      border-radius: 5px
+    }
+    `
   ]
 })
 export class HeroeComponent implements OnInit {
 
   heroe!: Heroe;
 
-  constructor(private activatedRoute: ActivatedRoute,private serviceHeroe: HeroesService) { }
+  constructor(private activatedRoute: ActivatedRoute,private serviceHeroe: HeroesService, private router: Router) { }
 
   ngOnInit(): void {
     this.activatedRoute.params
     .pipe(switchMap(({ id }) => this.serviceHeroe.getHeroe(id)))
     .subscribe(heroe => { this.heroe  = heroe; });
+  }
+
+  regresarListado(){
+    this.router.navigate(['/heroes/listado']);
   }
 }

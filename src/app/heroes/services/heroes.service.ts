7cb83613541652fe1bbd8325b70e6de142fcf2b3
Elvis Mora <pegazuz@live.com>
2021-04-07 19:01:42 -0500

    Configuración de variable global

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,20 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Heroe } from '../interfaces/heroes.interface';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
+  private urlApiHeroe: string = environment.baseUrlApiHeroe;
+
   constructor(private http: HttpClient) { }
 
 
   getHeroes():Observable<Heroe[]>{
-    return this.http.get<Heroe[]>('http://localhost:3000/heroes');
+    return this.http.get<Heroe[]>(`${this.urlApiHeroe}/heroes`);
   }
 
   getHeroe(id: string):Observable<Heroe>{
-    return this.http.get<Heroe>(`http://localhost:3000/heroes/${id}`);
+    return this.http.get<Heroe>(`${this.urlApiHeroe}/heroes/${id}`);
   }
 }

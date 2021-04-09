5fc685c71526729ec33387af932471afcbe41288
Elvis Mora <pegazuz@live.com>
2021-04-09 18:43:41 -0500

    Mapeo de interfaz para crear heroe

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { tap } from 'rxjs/operators';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-agregar',
@@ -9,6 +10,28 @@ import { tap } from 'rxjs/operators';
   ]
 })
 export class AgregarComponent implements OnInit {
+
+  //Objeto para el selector
+  publishers = [
+    {
+      id: 'DC Comics',
+      description: 'DC-Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      description: 'Marvel-Comics'
+    }
+  ]
+  
+  //Objeto para la imagen
+  heroeUrl: Heroe= {
+     alter_ego:'',
+     characters:'',
+     first_appearance:'',
+     publisher:Publisher.DCComics,
+     superhero:'',
+     alt_img:''
+  }
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
